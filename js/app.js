65f0ede655a18fa248745fafca194c62bc7b2ec0
David Bae <DavidBae@Davids-MacBook-Pro-2.local>
2019-08-05 13:05:45 -0500

    add comments clarifying how cpu works

--- js/app.js
+++ js/app.js
@@ -87,29 +87,37 @@ function cpuPlay() {
 
 function handleClick(evt) {
     let idx = evt.target.id;
+    // After we get our idx from the div id clean it
     let idxArr = cleanIdx(idx);
+    // If the game is in 2 player mode
     if(is2p) {
         if(isPlayerWhite) {
+            // If the tile is taken exit function
             if(board[idxArr[0]][idxArr[1]] != null) {
                 document.querySelector('.win').innerHTML = '<span class="dsp">TILE HAS BEEN CHOSEN!</span>';
                 return;
             } else {
+                // If tile is placed, switch turns and reset timer
                 document.querySelector('.win').innerHTML = '<span class="dsp"></span>';
                 board[idxArr[0]][idxArr[1]] = 'W';
-                // console.log(getPLS(evt));
                 timer = 15;
                 counter++;
+                // Push a deep copy of the board to our history array
                 history.push(board.map(inner => inner.slice())); 
                 render1(idxArr);
+                // Check if the move played was a winner
                 checkWhiteWin(idxArr[0], idxArr[1]);
+                // If it was a winner exit the function and display text
                 if(winnerPresent) {
                     document.querySelector('.win').innerHTML = `<span class="dsp">White Wins in ${counter} turns!</span>`;
                     return;
                 }
+                // Switch player
                 isPlayerWhite = false;
                 document.querySelector('.player').innerHTML = '<span class="etc">Player: Black\'s Turn</span>'
             }
         } else {
+            // Player Black
             if(board[idxArr[0]][idxArr[1]] != null) {
                 document.querySelector('.win').innerHTML = '<span class="dsp">TILE HAS BEEN CHOSEN!</span>';
                 return;
@@ -129,12 +137,16 @@ function handleClick(evt) {
                 document.querySelector('.player').innerHTML = 'Player: White\'s Turn'
             }
         }
+        // If the game is CPU
     } else {
+        // If player turn
         if(isPlayerWhite) {
+            // Same as above
             if(board[idxArr[0]][idxArr[1]] != null) {
                 document.querySelector('.win').innerHTML = '<span class="dsp">TILE HAS BEEN CHOSEN!</span>';
                 return;
             } else {
+                // Same as above
                 document.querySelector('.win').innerHTML = '<span class="dsp"></span>';
                 board[idxArr[0]][idxArr[1]] = 'W';
                 plsCache.push(getPLS(evt));
@@ -150,18 +162,27 @@ function handleClick(evt) {
                 isPlayerWhite = false;
                 document.querySelector('.player').innerHTML = '<span class="etc">Player: Black\'s Turn</span>'
             }
-            // CPU Action
+            // CPU Turn
+            // Retrieve the Player's Longest Sequence from the plsCache
             pls = plsCache[plsCache.length - 1].plsLength; 
+            // Retrieve the Computer's Longest Sequene from the clsCache.
+            // computer needs to constantly compare if it has cls already or if 
+            // cache has the cls
             cls = Math.max(cls, clsCache[clsCache.length - 1].clsLength);
+            // If the player has a pls greater than or equal to cls
             if(pls >= cls) {
+                // play defensively
                 defensiveAction();
+                // push the move the computer just palyed into the clsCache
                 clsCache.push(getCLS());
-                // console.log(getCLS());
                 counter++;
+                // Push a deep copy into the history array
                 history.push(board.map(inner => inner.slice()));
+                // Switch turns
                 isPlayerWhite = true;
                 document.querySelector('.player').innerHTML = 'Player: White\'s Turn'
             } else {
+                // If the computer has a cls greater than pls perform call agressive action
                 agressiveAction();
                 counter++;
                 history.push(board.map(inner => inner.slice()));
@@ -500,64 +521,83 @@ function filterArray(arr) {
     return answer;
 }
 /*----- CPU CODE -----*/ 
-// FIX UNDEFINED BUG
+// Retrieves the Player's Longest Sequence (PLS)
 function getPLS(evt) {
+    // We need to initialize multiple variables here
+    // counterPLS holds the current pls of the type e.g. -> row or column
     let counterPLS = 0;
+    // maxPLS is the maximum pls of all possible combinations
     let maxPLS = 0;
+    // blockLeft and blockRight indicate whether the current pls is blocke by 
+    // opposing tiles
     let blockLeft = false, blockRight = false;
+    // A temporary array to hold the indices of the pls
     let tempArr = [];
+    // The pls object that will be returned which contains all data on 
+    // pls length, the indices that make it up, and the type of pls it is.
     let objPLS = {
         plsLength: 0,
         plsIdx: [],
         plsType: '0'
     };
 
-    // Do I need to loop through our cells?
-    // Attempt without looping through cells
+    // Begin by cleaning our div id and returning a two element array [row, col]
     let idxArr = cleanIdx(evt.target.id);
+    
+    // Start at our row right
     for(let i = idxArr[1]; i < board.length; i++) {
+        // If the tile right if i is 'W' add one to our counter and add its indices
+        // to tempArr
         if(board[idxArr[0]][i] == 'W') {
             counterPLS++;
             tempArr.push([idxArr[0], i]);
         }
+        // If it is interrupted by a null break
         else if(board[idxArr[0]][i] != 'W') {
+            // If it is interrupted by a black tile, set blocked right to true and break
             if(board[idxArr[0]][i] == 'B')
                 blockRight = true;
             break;    
         }
     }
 
+    //Start at tile left of initial tile
     for(let i = idxArr[1] - 1; i >= 0; i--) {
+        // If tiles left of i - 1 are 'W' add one to our counter and add its indices
         if(board[idxArr[0]][i] == 'W') {
             counterPLS++;
             tempArr.push([idxArr[0], i]);
         }
+        // Refer to above
         else if(board[idxArr[0]][i] != 'W') {
+            // If it is interrupted by a black tile, set blocked left to true and break
             if(board[idxArr[0]][i] ==  'B')
                 blockLeft = true;
             break;
         }
     }
 
-    // If both left and right are blocked then it is not a
-    // valid PLS. Reset counters and idx array
-
+    // If both blockLeft and blockRight are blocked it is not a valid pls
+    // reset our counterPLS and remove our indices since it is not a row-threat
     if(blockLeft && blockRight) {
         counterPLS = 0;
         tempArr = [];
     }
 
+    // If it is not blocked
     // Reset our flags for the next check
     blockLeft = false, blockRight = false;
-    // Our store our counter pls
+    // Store the current max pls
     maxPLS = counterPLS;
     // In case our array has duplicates, filter it
     objPLS.plsIdx = filterArray(tempArr);
+    // Preemptively set the type to Row
     objPLS.plsType = 'R'
 
-    // Since we are checking our columns now reset 
+    // Before checking columns reset our counters, now that we have saved them
     counterPLS = 0;
     tempArr = [];
+    // Check tiles beneath i
     for(let i = idxArr[0]; i < board.length; i++) {
         if(board[i][idxArr[1]] == 'W') {
             counterPLS++;
@@ -569,7 +609,7 @@ function getPLS(evt) {
             break;
         }
     }
-
+    // Check tiles above i + 1
     for(let i = idxArr[0] - 1; i >= 0; i--) {
         if(board[i][idxArr[1]] == 'W') {
             counterPLS++;
@@ -581,21 +621,28 @@ function getPLS(evt) {
             break;
         }
     }
-
+    // Refer above
     if(blockLeft && blockRight) {
         counterPLS = 0;
         tempArr = [];
     }
-    
+    // If the maxPLS is less than the pls of the current type
     if(maxPLS < counterPLS) {
+        // Set maxPLS to counterPLS
         maxPLS = counterPLS;
+        // Our array of indices must also be replaced
         objPLS.plsIdx = filterArray(tempArr);
+        // Then replace the type of pls to 'C'
         objPLS.plsType = 'C'
     }
     
+    // Again reset our counters and block checkers
     blockLeft = false, blockRight = false;
     tempArr = [];
     counterPLS = 0;
+
+    // Check our diag now
+    // Check for all tiles going bottom left to top right to i 
     for(let i = idxArr[0], j = idxArr[1]; i < board.length; i++, j--) {
         if(board[i][j] == 'W') {
             counterPLS++;
@@ -607,6 +654,7 @@ function getPLS(evt) {
             break;
         }
     }
+    // Check all tiles going from i to bottom left and top right to end of board
     for(let i = idxArr[0] - 1, j = idxArr[1] + 1; i >= 0; i--, j++) {
         if(board[i][j] == 'W') {
             counterPLS++;
@@ -627,10 +675,13 @@ function getPLS(evt) {
         objPLS.plsIdx = filterArray(tempArr);
         objPLS.plsType = 'D'
     }
-
+    // Reset counters and block checks
     blockLeft = false, blockRight = false;
     tempArr = [];
     counterPLS = 0;
+
+    // Check anti diag now
+    // Check for all tiles above left of i.
     for(let i = idxArr[0], j = idxArr[1]; i >= 0; i--, j--) {
         if(board[i][j] == 'W') {
             counterPLS++;
@@ -642,6 +693,7 @@ function getPLS(evt) {
             break;
         }
     }
+    // Check for all tiles going bottom right of i
     for(let i = idxArr[0] + 1, j = idxArr[1] + 1; i < board.length; i++, j++) {
         if(board[i][j] == 'W') {
             counterPLS++;
@@ -662,12 +714,12 @@ function getPLS(evt) {
         objPLS.plsIdx = filterArray(tempArr);
         objPLS.plsType = 'AD'
     }
-
-    blockLeft = false, blockRight = false;
+    // Set the pls length equal to the max pls of all combinations
     objPLS.plsLength = maxPLS;
+    // Return the object
     return objPLS;
 }
-
+// COMPLETE REWRITE MUST BE DONE
 function getCLS() {
     let counterCLS = 0;
     let maxCLS = 0;
@@ -834,31 +886,42 @@ function getCLS() {
     objCLS.clsIdx = objCLS.clsIdx.sort();
     return objCLS;
 }
-// INCOMPLETE -> FIX UNDEFINED IT IS LINKED TO GETPLS
+// The defensive move the CPU can perform
 function defensiveAction() {
+    // Begin by getting the current pls
     let plsObj = plsCache[plsCache.length - 1];
+    // Isolate the array of indices for ease of access
     let plsArr = plsObj.plsIdx;
     console.log(plsObj);
 
+    // Switch to check what type pls is.
     switch(plsObj.plsType) {
-        case 'undefined':
-            console.log('i fucked up')
-            break;
+        // If the pls is a row
         case 'R':
+            // Begin by sorting the array of indices so that the y axis is in ascending order
             plsArr = plsArr.sort((a,b) => a[1] - b[1]);
+            // We are only concerned with the first and last indices of the pls, because we
+            // must block on either end
             let left = plsArr[0];
             let right = plsArr[plsArr.length - 1];
+            // Begin by prioritizing the left side of the pls. If the tile left of pls is null
             if(board[left[0]][left[1] - 1] == null) {
+                // Set the left tile to null
                 board[left[0]][left[1] - 1] = 'B';
+                // Render that tile
                 render2([left[0], left[1] - 1]);
+                // Push that move into the clsCache so the computer can retrieve its cls
                 clsMoves.push([left[0], left[1] - 1]);
+            // If the left side is already blocked, go right
             } else if(board[right[0]][right[1] + 1] == null) {
                 board[right[0]][right[1] + 1] = 'B';
                 render2([right[0], right[1] + 1]);
                 clsMoves.push([right[0], right[1] + 1]);
             }
             break;
+        // If the pls type is a column
         case 'C':
+            // Sort the indices by the x axis in ascending order
             plsArr = plsArr.sort((a,b) => a[0] - b[0]);
             let top = plsArr[0];
             let bottom = plsArr[plsArr.length - 1];
@@ -872,7 +935,9 @@ function defensiveAction() {
                 clsMoves.push([bottom[0] + 1, bottom[1]]);
             }
             break;
+        // If the type is an anti-diagonal
         case 'AD':
+            // Sort the indices by the x axis in ascending order
             plsArr = plsArr.sort((a,b) => a[0] - b[0]);
             let topLeft = plsArr[0];
             let bottomRight = plsArr[plsArr.length - 1];
@@ -886,7 +951,9 @@ function defensiveAction() {
                 clsMoves.push([bottomRight[0] + 1, bottomRight[1] + 1]);
             }
             break;
+        // If the type is a diagonal
         case 'D':
+            // Sort the indices by the x axis in ascending order
             plsArr = plsArr.sort((a,b) => a[0] - b[0]);
             let topRight = plsArr[0];
             let bottomLeft = plsArr[plsArr.length - 1];
@@ -902,7 +969,7 @@ function defensiveAction() {
             break;
     }
 }
-
+// COMPLETELY DEPENDANT ON GETCLS
 function agressiveAction() {
     let clsObj = clsCache[clsCache.length - 1];
     let clsArr = clsObj.clsIdx;
